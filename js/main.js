66a78c4e5022a1e97eb7b6337a1a70066b2d7954
Kate Bennet <kate.bennet@gmail.com>
2015-07-04 16:42:40 -0700

    Adding text to portfolio popups
    TO DO: Add text and correct images to each item.

--- js/main.js
+++ js/main.js
@@ -48,6 +48,11 @@ $(document).ready(function() {
         }
     });
 
+    /* Opening popup */
+    $('.open-popup-link').magnificPopup({
+    type:'inline',
+        midClick: true // allow opening popup on middle mouse click. Always set it to true  if you don't provide alternative source.
+    });
 
     /* Sticky menu */
     $(".navbar").sticky({
